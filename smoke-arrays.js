b73b4c407ef130ee01581de819c16cd2d1d9658e
natyaadmira <35670024+natyaadmira@users.noreply.github.com>
2018-02-18 23:47:08 -0800

    Update smoke-arrays.js

--- smoke-arrays.js
+++ smoke-arrays.js
@@ -13,7 +13,7 @@ function setup() {
 		y[i]= random(20,390)
 		r[i] = random(0,0.1)
 		yspeed[i] = random (3,5)
-		rspeed[i] = random(0, 0.05)
+		rspeed[i] = random(0.05,0.1)
 	}
 }
   
@@ -22,17 +22,18 @@ function draw() {
   noStroke();
 
 	for(i=0;i<=100;i=i+1){
+		
 		push()
 		fill(y[i]);
-		rect(x[i], y[i], 20, 20);
-		rotate(rspeed[i])
+		translate(x[i],y[i])
+		rotate(r[i])
+		rect(0, 0, 20, 20);
 		y[i]=y[i]-yspeed[i]
+		r[i] = r[i] + rspeed[i]
 		pop()
-		
 		
 			if (y[i] < -100) {
 				y[i]= random(20,390);
 			}
 	}
-			
 }
